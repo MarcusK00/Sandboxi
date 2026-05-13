359d5e69a331bf8090ccabe6090757c849a36d19
Marcus <marcuskristensen10@hotmail.com>
2026-05-13 18:30:15 +0200

    added obisian and oil id's to setMaterial function in game.ts

--- src/engine/game.ts
+++ src/engine/game.ts
@@ -16,9 +16,11 @@ export class Game {
   if (id === "water") this.input.setMaterial(CellType.Water);
   if (id === "glitch") this.input.setMaterial(CellType.Glitch);
   if (id === "electric") this.input.setMaterial(CellType.Electric);
-    if (id === "metal") this.input.setMaterial(CellType.Metal);
-    if (id === "lava") this.input.setMaterial(CellType.Lava);
-    if (id === "fire") this.input.setMaterial(CellType.Fire);
+  if (id === "metal") this.input.setMaterial(CellType.Metal);
+  if (id === "lava") this.input.setMaterial(CellType.Lava);
+  if (id === "fire") this.input.setMaterial(CellType.Fire);
+  if (id === "obsidian") this.input.setMaterial(CellType.Obsidian);
+  if (id === "oil") this.input.setMaterial(CellType.Oil);
 });
     this.input = new Input(this.renderer.canvas, this.simulation);
   }
