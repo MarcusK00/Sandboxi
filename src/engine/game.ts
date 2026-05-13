ceedf2bd5e26c04c5536160a45a36512012589d7
Marcus <marcuskristensen10@hotmail.com>
2026-05-13 12:29:07 +0200

    chore: created class to handle logic about game initialization. Used in main.ts

---
+++ src/engine/game.ts
@@ -0,0 +1,34 @@
+import { Simulation } from "./simulation";
+import { Renderer } from "./renderer";
+import { Input } from "./input";
+import { CellType } from "./types";
+
+export class Game {
+  simulation!: Simulation;
+  renderer!: Renderer;
+  input!: Input;
+
+  async init() {
+    this.simulation = new Simulation(320, 180);
+    this.renderer = await Renderer.create(this.simulation,(id) => {
+  if (id === "sand")  this.input.setMaterial(CellType.Sand);
+  if (id === "stone") this.input.setMaterial(CellType.Stone);
+  if (id === "water") this.input.setMaterial(CellType.Water);
+  if (id === "glitch") this.input.setMaterial(CellType.Glitch);
+  if (id === "electric") this.input.setMaterial(CellType.Electric);
+});
+    this.input = new Input(this.renderer.canvas, this.simulation);
+  }
+
+  start() {
+    const frame = () => {
+      this.simulation.update();
+      this.renderer.render(this.simulation);
+      requestAnimationFrame(frame);
+    };
+
+    frame();
+  }
+
+
+}
