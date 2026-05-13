b9eb2a61a32c8bfd76bbfadc020718e218d8ac88
Marcus <marcuskristensen10@hotmail.com>
2026-05-13 18:30:46 +0200

    added tick rate to simulation.ts

--- src/engine/simulation.ts
+++ src/engine/simulation.ts
@@ -6,11 +6,13 @@ export class Simulation {
   readonly grid: Grid;  
   width: number;
   height: number;
+  private tick = 0;
 
 constructor(width: number, height: number) {
   this.width = width;
   this.height = height;
   this.grid = new Grid(width, height);
+  
 }
 
 place(x: number, y: number, type: number) {
@@ -29,17 +31,29 @@ if (
         case CellType.Electric:
        this.grid.setMeta(x, y, 5);
     break;
+        case CellType.Fire:
+       this.grid.setMeta(x, y, 2);
+    break;
   }
 }
 
 update() {
-  for (let y = this.height - 2; y >= 0; y--) {
-    for (let x = 0; x < this.width; x++) {
-      const cell = this.grid.get(x, y);
-      updateFns[cell]?.(this.grid, x, y);
+    const leftToRight = (this.tick++ % 2) === 0;
+
+    for (let y = this.height - 2; y >= 0; y--) {
+      if (leftToRight) {
+        for (let x = 0; x < this.width; x++) {
+          const cell = this.grid.get(x, y);
+          updateFns[cell]?.(this.grid, x, y);
+        }
+      } else {
+        for (let x = this.width - 1; x >= 0; x--) {
+          const cell = this.grid.get(x, y);
+          updateFns[cell]?.(this.grid, x, y);
+        }
+      }
     }
   }
-}
 
 reset(){
     this.grid.cells.fill(CellType.Empty);
