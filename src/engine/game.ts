b2ff4044a32a8b2a86131771ebe4ed5a49c7fa9b
Marcus <marcuskristensen10@hotmail.com>
2026-05-13 14:22:45 +0200

    feature: Added 3 new elements. Metal, Lava & Fire. Fire still needs work.

--- src/engine/game.ts
+++ src/engine/game.ts
@@ -16,6 +16,9 @@ export class Game {
   if (id === "water") this.input.setMaterial(CellType.Water);
   if (id === "glitch") this.input.setMaterial(CellType.Glitch);
   if (id === "electric") this.input.setMaterial(CellType.Electric);
+    if (id === "metal") this.input.setMaterial(CellType.Metal);
+    if (id === "lava") this.input.setMaterial(CellType.Lava);
+    if (id === "fire") this.input.setMaterial(CellType.Fire);
 });
     this.input = new Input(this.renderer.canvas, this.simulation);
   }
