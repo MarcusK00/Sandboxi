05eec98c6ddf17471250ac10988281fb1d21b932
Marcus <marcuskristensen10@hotmail.com>
2026-05-23 15:52:51 +0200

    chore: Formatted for readability. Also added everything needed for the 6 new elements to work in simulation.ts Smoke and Steam also is using setMeta.

--- src/engine/simulation.ts
+++ src/engine/simulation.ts
@@ -3,41 +3,47 @@ import { updateFns } from "../materials/materials";
 import { CellType } from "./types";
 
 export class Simulation {
-  readonly grid: Grid;  
+  readonly grid: Grid;
   width: number;
   height: number;
   private tick = 0;
 
-constructor(width: number, height: number) {
-  this.width = width;
-  this.height = height;
-  this.grid = new Grid(width, height);
-  
-}
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+    this.grid = new Grid(width, height);
 
-place(x: number, y: number, type: number) {
-  if (!this.grid.inBounds(x, y)) return;
-
-if (
-  type !== CellType.Empty &&
-  this.grid.get(x, y) > 0
-) return;
-  this.grid.set(x, y, type);
-
-  switch(type){
-    case CellType.Glitch:
-        this.grid.setMeta(x, y, 5);
-    break;
-        case CellType.Electric:
-       this.grid.setMeta(x, y, 5);
-    break;
-        case CellType.Fire:
-       this.grid.setMeta(x, y, 1);
-    break;
   }
-}
 
-update() {
+  place(x: number, y: number, type: number) {
+    if (!this.grid.inBounds(x, y)) return;
+
+    if (
+      type !== CellType.Empty &&
+      this.grid.get(x, y) > 0
+    ) return;
+    this.grid.set(x, y, type);
+
+    switch (type) {
+      case CellType.Glitch:
+        this.grid.setMeta(x, y, 5);
+        break;
+      case CellType.Electric:
+        this.grid.setMeta(x, y, 5);
+        break;
+      case CellType.Fire:
+        this.grid.setMeta(x, y, 1);
+        break;
+      case CellType.Smoke:
+        this.grid.setMeta(x, y, 2);
+        break;
+      case CellType.Steam:
+        this.grid.setMeta(x, y, 9);
+        break;
+    }
+  }
+
+  update() {
     const leftToRight = (this.tick++ % 2) === 0;
 
     for (let y = this.height - 2; y >= 0; y--) {
@@ -55,7 +61,7 @@ update() {
     }
   }
 
-reset(){
+  reset() {
     this.grid.cells.fill(CellType.Empty);
-}
+  }
 }
