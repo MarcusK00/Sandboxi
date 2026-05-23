cd8d8cc9a7d2b62ee1076c8963127f3f46cba96d
Marcus <marcuskristensen10@hotmail.com>
2026-05-23 20:07:23 +0200

    chore: mud behaviour with water fix.

--- src/materials/mud.ts
+++ src/materials/mud.ts
@@ -4,37 +4,30 @@ import { CellType } from "../engine/types";
 export function updateMud(grid: Grid, x: number, y: number) {
     if (y + 1 >= grid.height) return;
 
-    const below = grid.get(x, y + 1);
+    switch (grid.get(x, y + 1)) {
+        case CellType.Empty: {
+            if (Math.random() > 0.45) return;
+            grid.swap(x, y, x, y + 1);
+            break;
+        }
+        case CellType.Water: {
+            if (Math.random() > 0.08) return;
+            grid.swap(x, y, x, y + 1);
 
-
-    if (below === CellType.Empty) {
-        if (Math.random() > 0.45) return;
-
-        grid.swap(x, y, x, y + 1);
-        return;
-    }
-
-
-    if (below === CellType.Water) {
-
-        if (Math.random() > 0.08) return;
-
-        grid.swap(x, y, x, y + 1);
-        return;
-    }
-
-
-    if (Math.random() > 0.985) {
-        const dir = Math.random() < 0.5 ? -1 : 1;
-        const nx = x + dir;
-
-        if (
-            grid.inBounds(nx, y + 1) &&
-            grid.get(nx, y) === CellType.Empty &&
-            grid.get(nx, y + 1) === CellType.Empty
-        ) {
-            grid.swap(x, y, nx, y + 1);
-            return;
+            break;
+        }
+        default: {
+            if (Math.random() > 0.985) return;
+            const dir = Math.random() < 0.5 ? -1 : 1;
+            const nx = x + dir;
+            if (
+                grid.inBounds(nx, y + 1) &&
+                grid.get(nx, y) === CellType.Empty &&
+                grid.get(nx, y + 1) === CellType.Empty
+            ) {
+                grid.swap(x, y, nx, y + 1);
+            }
+            break;
         }
     }
 }
